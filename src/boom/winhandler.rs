671a8a7c35aa5679ba7a24612cde6992091338ee
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2023-12-31 23:03:16 -0500

    Oh my god

--- src/boom/winhandler.rs
+++ src/boom/winhandler.rs
@@ -99,5 +99,62 @@ impl WinHandler {
   ///
   /// Consider this glfw's glfwPollEvents but not.
   ///
-  pub fn poll(&self) {}
+  pub fn poll(&self) {
+    let mut event_pump = self.sdl_context.as_ref().unwrap().event_pump().unwrap();
+
+    for event in event_pump.poll_iter() {
+      match event  {
+        event::Event::Quit { timestamp } => todo!(),
+        event::Event::AppTerminating { timestamp } => todo!(),
+        event::Event::AppLowMemory { timestamp } => todo!(),
+        event::Event::AppWillEnterBackground { timestamp } => todo!(),
+        event::Event::AppDidEnterBackground { timestamp } => todo!(),
+        event::Event::AppWillEnterForeground { timestamp } => todo!(),
+        event::Event::AppDidEnterForeground { timestamp } => todo!(),
+        event::Event::Display { timestamp, display_index, display_event } => todo!(),
+        event::Event::Window { timestamp, window_id, win_event } => todo!(),
+        event::Event::KeyDown { timestamp, window_id, keycode, scancode, keymod, repeat } => todo!(),
+        event::Event::KeyUp { timestamp, window_id, keycode, scancode, keymod, repeat } => todo!(),
+        event::Event::TextEditing { timestamp, window_id, text, start, length } => todo!(),
+        event::Event::TextInput { timestamp, window_id, text } => todo!(),
+        event::Event::MouseMotion { timestamp, window_id, which, mousestate, x, y, xrel, yrel } => todo!(),
+        event::Event::MouseButtonDown { timestamp, window_id, which, mouse_btn, clicks, x, y } => todo!(),
+        event::Event::MouseButtonUp { timestamp, window_id, which, mouse_btn, clicks, x, y } => todo!(),
+        event::Event::MouseWheel { timestamp, window_id, which, x, y, direction, precise_x, precise_y } => todo!(),
+        event::Event::JoyAxisMotion { timestamp, which, axis_idx, value } => todo!(),
+        event::Event::JoyBallMotion { timestamp, which, ball_idx, xrel, yrel } => todo!(),
+        event::Event::JoyHatMotion { timestamp, which, hat_idx, state } => todo!(),
+        event::Event::JoyButtonDown { timestamp, which, button_idx } => todo!(),
+        event::Event::JoyButtonUp { timestamp, which, button_idx } => todo!(),
+        event::Event::JoyDeviceAdded { timestamp, which } => todo!(),
+        event::Event::JoyDeviceRemoved { timestamp, which } => todo!(),
+        event::Event::ControllerAxisMotion { timestamp, which, axis, value } => todo!(),
+        event::Event::ControllerButtonDown { timestamp, which, button } => todo!(),
+        event::Event::ControllerButtonUp { timestamp, which, button } => todo!(),
+        event::Event::ControllerDeviceAdded { timestamp, which } => todo!(),
+        event::Event::ControllerDeviceRemoved { timestamp, which } => todo!(),
+        event::Event::ControllerDeviceRemapped { timestamp, which } => todo!(),
+        event::Event::ControllerTouchpadDown { timestamp, which, touchpad, finger, x, y, pressure } => todo!(),
+        event::Event::ControllerTouchpadMotion { timestamp, which, touchpad, finger, x, y, pressure } => todo!(),
+        event::Event::ControllerTouchpadUp { timestamp, which, touchpad, finger, x, y, pressure } => todo!(),
+        event::Event::FingerDown { timestamp, touch_id, finger_id, x, y, dx, dy, pressure } => todo!(),
+        event::Event::FingerUp { timestamp, touch_id, finger_id, x, y, dx, dy, pressure } => todo!(),
+        event::Event::FingerMotion { timestamp, touch_id, finger_id, x, y, dx, dy, pressure } => todo!(),
+        event::Event::DollarGesture { timestamp, touch_id, gesture_id, num_fingers, error, x, y } => todo!(),
+        event::Event::DollarRecord { timestamp, touch_id, gesture_id, num_fingers, error, x, y } => todo!(),
+        event::Event::MultiGesture { timestamp, touch_id, d_theta, d_dist, x, y, num_fingers } => todo!(),
+        event::Event::ClipboardUpdate { timestamp } => todo!(),
+        event::Event::DropFile { timestamp, window_id, filename } => todo!(),
+        event::Event::DropText { timestamp, window_id, filename } => todo!(),
+        event::Event::DropBegin { timestamp, window_id } => todo!(),
+        event::Event::DropComplete { timestamp, window_id } => todo!(),
+        event::Event::AudioDeviceAdded { timestamp, which, iscapture } => todo!(),
+        event::Event::AudioDeviceRemoved { timestamp, which, iscapture } => todo!(),
+        event::Event::RenderTargetsReset { timestamp } => todo!(),
+        event::Event::RenderDeviceReset { timestamp } => todo!(),
+        event::Event::User { timestamp, window_id, type_, code, data1, data2 } => todo!(),
+        event::Event::Unknown { timestamp, type_ } => todo!(),
+    }
+    }
+  }
 }
