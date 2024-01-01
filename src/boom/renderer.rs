18758adf1f18c5f2f371a09945429605f1f7baf8
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2024-01-01 09:27:20 -0500

    Time to get crazy

--- src/boom/renderer.rs
+++ src/boom/renderer.rs
@@ -1,6 +1,9 @@
 use std::mem::swap;
 
-use glam::{DVec2, IVec2};
+use rayon::prelude::*;
+
+use glam::{DVec2, IVec2, IVec4};
+use rand::Rng;
 use sdl2::pixels::PixelFormatEnum;
 
 use super::{win_handler::WinHandler, world::World};
@@ -38,14 +41,14 @@ impl Renderer {
     let mut draw_pixel = |x: usize, y: usize, r: u8, b: u8, g: u8, a: u8| {
       let index = y * pitch + x * 4;
 
-      buffer[index] = x as u8;
-      buffer[index + 1] = y as u8;
-      buffer[index + 2] = 0;
-      buffer[index + 3] = 255;
+      buffer[index] = r;
+      buffer[index + 1] = g;
+      buffer[index + 2] = b;
+      buffer[index + 3] = a;
     };
 
     // This closure is from: https://github.com/ssloy/tinyrenderer/wiki/Lesson-1:-Bresenham%E2%80%99s-Line-Drawing-Algorithm#timings-fifth-and-final-attempt
-    let draw_line = |_x0: i32, _y0: i32, _x1: i32, _y1: i32, r: u8, g: u8, b: u8, a: u8| {
+    let mut draw_line = |_x0: i32, _y0: i32, _x1: i32, _y1: i32, r: u8, g: u8, b: u8, a: u8| {
       let mut x0 = _x0;
       let mut x1 = _x1;
       let mut y0 = _y0;
@@ -106,7 +109,7 @@ impl Renderer {
         },
       );
 
-      let mut perp_wall_dist = 0.0;
+      let perp_wall_dist;
 
       let mut step = IVec2::new(0, 0);
 
@@ -153,6 +156,54 @@ impl Renderer {
       }
 
       let line_height = (h as f64 / perp_wall_dist).floor() as i32;
+
+      let mut draw_start = -line_height / 2 + h / 2;
+      if draw_start < 0 {
+        draw_start = 0
+      }
+      let mut draw_end = line_height / 2 + h / 2;
+      if draw_end >= h {
+        draw_end = h - 1;
+      }
+
+      let mut r: u8 = 0;
+      let mut g: u8 = 0;
+      let mut b: u8 = 0;
+      let mut a: u8 = 255;
+
+      match world.map.data[map_pos.x as usize][map_pos.y as usize] {
+        1 => {
+          r = 255;
+        }
+        2 => {
+          g = 255;
+        }
+        3 => {
+          b = 255;
+        }
+        4 => {
+          r = 255;
+          g = 255;
+        }
+        _ => (),
+      }
+
+      if side == 1 {
+        r /= 2;
+        g /= 2;
+        b /= 2;
+      }
+
+      // draw_line(x, draw_start, x, draw_end, r, g, b, a);
+    }
+
+    let mut random = rand::thread_rng();
+    let mut cool = || -> u8 { random.gen_range(0..255) as u8 };
+
+    for y in 0..window_size.y {
+      for x in 0..window_size.x {
+        draw_pixel(x as usize, y as usize, cool(), cool(), cool(), cool());
+      }
     }
   }
 
