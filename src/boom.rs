0e97bcf9b16b619f6019b46e391038ee97e6c3b2
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2023-12-31 20:50:56 -0500

    Boom now has some basic logic

--- src/boom.rs
+++ src/boom.rs
@@ -1,17 +1,47 @@
+use spin_sleep::LoopHelper;
+
 use self::win::Win;
 
 mod win;
 
 pub struct Boom {
   window: Win,
+  should_close: bool,
+  loop_helper: LoopHelper,
+  delta: f64,
 }
 
 impl Boom {
   pub fn new() -> Self {
-    return Boom { window: Win::new() };
+    return Boom {
+      window: Win::new(),
+      should_close: false,
+      loop_helper: LoopHelper::builder().build_with_target_rate(60.0),
+      delta: 0.0,
+    };
   }
 
-  pub fn main(&mut self) {}
+  ///
+  /// Main logic of Boom.
+  /// 
+  fn main(&mut self) {
+    self.delta = self.loop_helper.loop_start_s();
+
+    self
+      .window
+      .change_title(format!("Boom | delta: {}", self.delta).as_str());
+
+    self.loop_helper.loop_sleep();
+  }
+
+  ///
+  /// Entry point to Boom.
+  /// 
+  pub fn enter_main_loop(&mut self) {
+    while !self.should_close {
+      self.main();
+    }
+  }
 }
 
 impl Drop for Boom {
