6371bdc51a7ab29655c7c9ef9dc0c24252b1c28f
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2024-01-01 09:46:21 -0500

    Make the window say the delta time

--- src/boom.rs
+++ src/boom.rs
@@ -1,3 +1,5 @@
+use std::time::Duration;
+
 use spin_sleep::LoopHelper;
 
 use self::{renderer::Renderer, win_handler::WinHandler, world::World};
@@ -13,6 +15,7 @@ pub struct Boom {
   should_close: bool,
   loop_helper: LoopHelper,
   delta: f64,
+  current_fps: f64,
 }
 
 impl Boom {
@@ -22,9 +25,21 @@ impl Boom {
       renderer: Renderer::new(),
       world: World::new(),
       should_close: false,
-      loop_helper: LoopHelper::builder().build_with_target_rate(60.0),
+      loop_helper: LoopHelper::builder()
+        .report_interval(Duration::new(1, 0))
+        .build_with_target_rate(60.0),
       delta: 0.0,
+      current_fps: 0.0,
     };
+  }
+
+  ///
+  /// Automatically poll the FPS counter from spin_sleep.
+  ///
+  fn update_fps(&mut self) {
+    if let Some(fps) = self.loop_helper.report_rate() {
+      self.current_fps = fps;
+    }
   }
 
   ///
@@ -41,9 +56,11 @@ impl Boom {
 
     // self.world.on_tick(self.delta);
 
+    self.update_fps();
+
     self
       .window
-      .change_title(format!("Boom | delta: {}", self.delta).as_str());
+      .change_title(format!("Boom | delta: {} | FPS: {}", self.delta, self.current_fps).as_str());
 
     self.renderer.draw(&mut self.window, &mut self.world);
 
