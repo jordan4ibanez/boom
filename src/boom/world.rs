b9d6249ae5f24400f8e80b4b5d5a7f10cd54efed
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2023-12-31 23:45:42 -0500

    Some basics

--- src/boom/world.rs
+++ src/boom/world.rs
@@ -1,3 +1,5 @@
+use std::collections::{hash_map, HashMap};
+
 use glam::Vec2;
 
 struct Mob {
@@ -22,5 +24,23 @@ struct Bullet {}
 impl Bullet {
   pub fn new() -> Self {
     Bullet {}
+  }
+}
+
+pub struct World {
+  mobs: Vec<Mob>,
+  bullet: HashMap<String, Bullet>,
+}
+
+impl World {
+  pub fn new() -> Self {
+    World {
+      mobs: vec![],
+      bullet: HashMap::new(),
+    }
+  }
+
+  pub fn on_tick(&mut self, delta: f64) {
+    println!("tick tock {}", delta)
   }
 }
